1c13b3ff8fcde81f156854c54d001f4ed0daf91c
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2026-04-29 11:11:20 +0530

    feat: implement account, auth, and transition API services using RTK Query

---
+++ mobile/services/account.api.js
@@ -0,0 +1,37 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// Define a service using a base URL and expected endpoints
+export const accountApi = createApi({
+  reducerPath: "accountApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3003/api/account",
+    credentials: "include",
+  }),
+  tagTypes: ["Account"],
+  endpoints: (build) => ({
+    createAccount: build.mutation({
+      query: (body) => ({
+        url: "/createAccount",
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: ["Account"],
+    }),
+    getAccount: build.query({
+      query: () => ({
+        url: "/getAccount",
+        method: "GET",
+      }),
+      providesTags: ["Account"],
+    }),
+    user: build.query({
+      query: () => ({
+        url: "/user",
+        method: "GET",
+      }),
+    }),
+  }),
+});
+
+export const { useCreateAccountMutation, useGetAccountQuery, useProfileQuery } =
+  accountApi;
